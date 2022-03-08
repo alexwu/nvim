5600afc46bd7d0d4f147f400027fd7a359ce9f2b
Alex Wu <adwu@pm.me>
2022-03-07 21:15:01 -0800

    Writes autcommand for chezmoi apply using vim.notify

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,4 +8,28 @@ require("snazzy").setup("dark")
 
 require("plugins.treesitter")
 
-vim.cmd([[autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"]])
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = os.getenv("HOME") .. "/.local/share/chezmoi/*",
+	callback = function()
+		vim.schedule(function()
+			local Job = require("plenary.job")
+
+			Job
+				:new({
+					command = "chezmoi",
+					args = { "apply" },
+					cwd = vim.loop.cwd(),
+					on_stderr = function(_, data)
+						vim.notify(data, "error")
+					end,
+					on_stdout = function(_, return_val)
+						vim.notify(return_val)
+					end,
+					on_exit = function(_, _)
+						vim.notify("chezmoi apply: successful")
+					end,
+				})
+				:sync()
+		end)
+	end,
+})
