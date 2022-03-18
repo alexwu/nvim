47473a880a95fd097d3e92f7fc46b08badcdce84
Alex Wu <adwu@pm.me>
2022-03-17 21:16:11 -0700

    adds hop locals map

--- dot_config/nvim/lua/plugins/hop/init.lua
+++ dot_config/nvim/lua/plugins/hop/init.lua
@@ -4,33 +4,25 @@ local extension = require("hop-extensions")
 
 hop.setup({})
 
-set("n", "s", function()
-  hop.hint_words()
+set({ "n", "o" }, "s", function()
+	hop.hint_words()
 end)
-set("n", "gs", function()
-  hop.hint_words()
+
+set("n", "go", function()
+	extension.hint_locals()
 end)
 
 set("n", "gl", function()
-  hop.hint_lines_skip_whitespace()
+	hop.hint_lines_skip_whitespace()
 end)
-
--- set("n", "<C-h>", function()
---   hop.hint_words({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR, current_line_only = true })
--- end, {})
---
--- set("n", "<C-l>", function()
---   hop.hint_words({ direction = require("hop.hint").HintDirection.AFTER_CURSOR, current_line_only = true })
--- end, {})
-
-set("n", "f", function()
-  hop.hint_char1({ direction = require("hop.hint").HintDirection.AFTER_CURSOR, current_line_only = true })
-end, {})
-
-set("n", "F", function()
-  hop.hint_char1({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR, current_line_only = true })
-end, {})
 
 set("n", "go", function()
-  extension.hint_textobjects()
+	extension.hint_textobjects()
 end)
+set({ "n", "o" }, "f", function()
+	hop.hint_char1({ direction = require("hop.hint").HintDirection.AFTER_CURSOR, current_line_only = true })
+end, {})
+
+set({ "n", "o" }, "F", function()
+	hop.hint_char1({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR, current_line_only = true })
+end, {})
