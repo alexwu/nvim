a01ba369bd7e8a4df7f5fd8b5fce161f91e19f21
Alex Wu <adwu@pm.me>
2022-09-17 09:31:17 -0700

    wip

--- dot_config/nvim/lua/bombeelu/just.lua
+++ dot_config/nvim/lua/bombeelu/just.lua
@@ -1,7 +1,20 @@
+local Terminal = require("toggleterm.terminal").Terminal
+
 local M = {}
 
 function M.setup()
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.api.nvim_buf_create_user_command(bufnr, "Just", function()
+    M.just()
+  end, {})
+end
 
+function M.just()
+  Terminal:new({
+    cmd = "just",
+    hidden = false,
+    start_in_insert = false,
+  }):toggle()
 end
 
 return M
