7c0a450a7c6080797a468d74d352a2ab916fa04f
Alex Wu <adwu@pm.me>
2022-09-20 21:34:15 -0700

    more updates to gitsigns mappings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -47,7 +47,7 @@ require("gitsigns").setup({
         return "]c"
       end
       vim.schedule(function()
-        gs.next_hunk()
+        gs.next_hunk({ navigation_message = false, preview = true })
       end)
       return "<Ignore>"
     end, { expr = true, desc = "Next Git hunk" })
@@ -57,7 +57,7 @@ require("gitsigns").setup({
         return "[c"
       end
       vim.schedule(function()
-        gs.prev_hunk()
+        gs.prev_hunk({ navigation_message = false, preview = true })
       end)
       return "<Ignore>"
     end, { expr = true, desc = "Previous Git hunk" })
