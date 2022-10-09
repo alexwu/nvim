2d8b448c48abc021945218f384ea69995f912fe9
Alex Wu <adwu@pm.me>
2022-10-08 20:41:44 -0700

    updates implementation of repeat

--- lua/bombeelu/repeat.lua
+++ lua/bombeelu/repeat.lua
@@ -1,31 +1,24 @@
--- Simple wrapper around https://github.com/tpope/vim-repeat
-
+-- -- Simple wrapper around https://github.com/tpope/vim-repeat
 local api = vim.api
 local M = {}
-
-local repeat_fn
-
-function M.repeat_action()
-  repeat_fn()
-end
 
 function M.mk_repeatable(fn)
   return function(...)
     local args = { ... }
     local nargs = select("#", ...)
-    repeat_fn = function()
+    vim.go.operatorfunc = "v:lua.require'bombeelu.repeat'.repeat_action"
+
+    M.repeat_action = function()
       fn(unpack(args, 1, nargs))
-      local vimfn = vim.fn
-      local sequence = string.format("%sBombeeluRepeat", api.nvim_replace_termcodes("<Plug>", true, true, true))
-      pcall(vimfn["repeat#set"], sequence, -1)
+      if vim.fn.exists("*repeat#set") == 1 then
+        local action =
+          api.nvim_replace_termcodes(string.format("<cmd>call %s()<cr>", vim.go.operatorfunc), true, true, true)
+        vim.fn["repeat#set"](action, -1)
+      end
     end
 
-    repeat_fn()
+    vim.cmd("normal g@l")
   end
 end
-
-vim.keymap.set("n", "<Plug>BombeeluRepeat", function()
-  M.repeat_action()
-end, { noremap = false, silent = true })
 
 return M
