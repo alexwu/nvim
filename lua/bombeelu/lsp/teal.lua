8c2ec420d4f11a8bc5eaecf8f9c2e94b1cc7fd95
Alex Wu <adwu@pm.me>
2023-07-27 20:13:10 -0700

    removes old files, re-enables just treesitter

--- lua/bombeelu/lsp/teal.lua
+++
@@ -1,9 +0,0 @@
-local lspconfig = require("lspconfig")
-
-local teal = {}
-
-function teal.setup(opts)
-  lspconfig.teal_ls.setup({ on_attach = opts.on_attach, capabilities = opts.capabilities })
-end
-
-return teal
