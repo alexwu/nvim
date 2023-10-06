bcb3612d255d5ffd75e7ceaf28c23ef382032231
Alex Wu <adwu@pm.me>
2023-10-05 18:06:25 -0700

    update to latest indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,19 +3,32 @@ if vim.g.goneovim then
 end
 
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = "VeryLazy",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("indent_blankline").setup({
-      use_treesitter = true,
-      show_current_context = true,
-      show_current_context_start = false,
-      context_highlight = "Label",
-      show_first_indent_level = false,
-      buftype_exclude = { "help", "fzf", "lspinfo", "NvimTree", "nofile" },
-      filetype_exclude = { "help", "fzf", "lspinfo", "NvimTree", "windline" },
-      char = "▏",
-    })
-  end,
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      scope = {
+        enabled = true,
+        show_start = false,
+      },
+    },
+    event = "VeryLazy",
+  },
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  --   config = function()
+  --     require("indent_blankline").setup({
+  --       use_treesitter = true,
+  --       show_current_context = true,
+  --       show_current_context_start = false,
+  --       context_highlight = "Label",
+  --       show_first_indent_level = false,
+  --       buftype_exclude = { "help", "fzf", "lspinfo", "NvimTree", "nofile" },
+  --       filetype_exclude = { "help", "fzf", "lspinfo", "NvimTree", "windline" },
+  --       char = "▏",
+  --     })
+  --   end,
+  -- },
 }
