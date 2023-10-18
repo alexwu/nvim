280b2208190bd570c1b6dc31ff786a09e3f8af16
Alex Wu <adwu@pm.me>
2023-10-17 20:17:25 -0700

    fix syntax

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -18,15 +18,14 @@ return {
       -- or add custom linters.
       ---@type table<string,table>
       -- linters = {
-        -- -- Example of using selene only when a selene.toml file is present
-        -- selene = {
-        --   -- `condition` is another LazyVim extension that allows you to
-        --   -- dynamically enable/disable linters based on the context.
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
-        --   end,
-        -- },
-      },
+      -- -- Example of using selene only when a selene.toml file is present
+      -- selene = {
+      --   -- `condition` is another LazyVim extension that allows you to
+      --   -- dynamically enable/disable linters based on the context.
+      --   condition = function(ctx)
+      --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
+      --   end,
+      -- },
     },
     config = function(_, opts)
       local M = {}
