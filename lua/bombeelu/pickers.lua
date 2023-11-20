abdc6556408f75a969449c3600fafa653fd51fb8
Alex Wu <adwu@pm.me>
2023-11-20 13:01:00 -0800

    removes some more files

--- lua/bombeelu/pickers.lua
+++ lua/bombeelu/pickers.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.config_files()
+---@diagnostic disable-next-line: undefined-field
   local config = vim.system({ "chezmoi", "source-path", "~/.config/nvim" }, { text = true }):wait()
 
   local cwd = string.gsub(config.stdout, "\n", "")
