9c6c979890ca00d06f3b0de591b332852e37042c
Alex Wu <adwu@pm.me>
2023-11-21 21:49:17 -0800

    updates noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,64 +1,65 @@
 return {
   "folke/noice.nvim",
   event = "VimEnter",
-  config = function()
-    require("noice").setup({
-      popupmenu = {
+  opts = {
+    popupmenu = {
+      enabled = true,
+      backend = "cmp",
+    },
+    notify = {
+      enabled = true,
+      view = "notify",
+    },
+    lsp = {
+      progress = {
         enabled = true,
-        backend = "cmp",
+        -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+        -- See the section on formatting for more details on how to customize.
+        --- @type NoiceFormat|string
+        format = "lsp_progress",
+        --- @type NoiceFormat|string
+        format_done = "lsp_progress_done",
+        throttle = 1000 / 30, -- frequency to update lsp progress message
+        view = "mini",
       },
-      notify = {
-        enabled = false,
-        view = "notify",
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
       },
-      lsp = {
-        progress = {
-          enabled = false,
-          -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
-          -- See the section on formatting for more details on how to customize.
-          --- @type NoiceFormat|string
-          format = "lsp_progress",
-          --- @type NoiceFormat|string
-          format_done = "lsp_progress_done",
-          throttle = 1000 / 30, -- frequency to update lsp progress message
-          view = "mini",
-        },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = false,
-          ["cmp.entry.get_documentation"] = true,
-        },
-        hover = {
+      hover = {
+        enabled = true,
+        silent = false, -- set to true to not show a message if hover is not available
+        view = nil, -- when nil, use defaults from documentation
+        ---@type NoiceViewOptions
+        opts = {}, -- merged with defaults from documentation
+      },
+      signature = {
+        enabled = true,
+        auto_open = {
           enabled = true,
-          silent = false, -- set to true to not show a message if hover is not available
-          view = nil, -- when nil, use defaults from documentation
-          ---@type NoiceViewOptions
-          opts = {}, -- merged with defaults from documentation
+          trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+          luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+          throttle = 50, -- Debounce lsp signature help request by 50ms
         },
-        signature = {
-          enabled = true,
-          auto_open = {
-            enabled = true,
-            trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-            luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-            throttle = 50, -- Debounce lsp signature help request by 50ms
-          },
-          view = nil, -- when nil, use defaults from documentation
-          ---@type NoiceViewOptions
-          opts = {}, -- merged with defaults from documentation
-        },
-        documentation = {
-          enabled = true,
-          view = "hover",
-        },
+        view = nil, -- when nil, use defaults from documentation
+        ---@type NoiceViewOptions
+        opts = {}, -- merged with defaults from documentation
       },
-      presets = {
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-        lsp_doc_border = true,
+      documentation = {
+        enabled = true,
+        view = "hover",
       },
-    })
+    },
+    presets = {
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = true,
+      lsp_doc_border = true,
+    },
+  },
+  config = function(_, opts)
+    require("noice").setup(opts)
 
     require("telescope").load_extension("noice")
   end,
