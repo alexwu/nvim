dcb77f261928a9f417e35493e0e6c4bfb1372d0f
Alex Wu <adwu@pm.me>
2023-12-21 14:57:52 -0800

    chore: Tweaks

--- lua/bombeelu/pickers.lua
+++ lua/bombeelu/pickers.lua
@@ -5,7 +5,7 @@ function M.config_files()
   local config = vim.system({ "chezmoi", "source-path", "~/.config/nvim" }, { text = true }):wait()
   local cwd = string.gsub(config.stdout, "\n", "")
 
-  require("nucleo").find({ cwd = cwd })
+  require("nucleo.sources").find_files({ cwd = cwd })
 end
 
 return M
