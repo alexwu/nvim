269606e1dbd4fbef8dcfa230d676425284d8ad10
Alex Wu <adwu@pm.me>
2024-06-25 14:20:37 -0700

    tweaks and reworks neotest config

--- lua/bombeelu/neotest.lua
+++ lua/bombeelu/neotest.lua
@@ -3,171 +3,93 @@ local M = {}
 function M.setup()
   local neotest = require("neotest")
 
-  neotest.setup({
-    adapters = {
-      require("neotest-rspec")({
-        rspec_cmd = function()
-          return vim
-            .iter({
-              "bundle",
-              "exec",
-              "rspec",
-            })
-            :flatten()
-            :totable()
-        end,
-      }),
-      require("neotest-rust")({
-        args = { "--no-capture" },
-        dap_adapter = "lldb",
-      }),
-      require("neotest-jest")({
-        jestCommand = "yarn test",
-        jestConfigFile = "jest.config.js",
-        env = { DEBUG_PRINT_LIMIT = 20000 },
-        cwd = function(path)
-          return vim.fs.dirname(
-            vim.fs.find(
-              { "jest.config.js", "package.json" },
-              { upward = true, stop = vim.loop.os_homedir(), path = path }
-            )[1]
-          )
-        end,
-      }),
-      require("neotest-vitest"),
-    },
-    icons = {
-      passed = " ✔",
-      running = " ",
-      failed = " ✖",
-      skipped = " ﰸ",
-      unknown = " ?",
-      non_collapsible = "─",
-      collapsed = "─",
-      expanded = "╮",
-      child_prefix = "├",
-      final_child_prefix = "╰",
-      child_indent = "│",
-      final_child_indent = " ",
-    },
-    status = { virtual_text = false },
-    output = { open_on_run = true },
-    quickfix = {
-      open = function()
-        vim.cmd("Trouble quickfix")
-      end,
-    },
-    watch = {
-      enabled = false,
-    },
-  })
-
-  local wk = require("which-key")
-  wk.register({
-    t = {
-      name = "+test",
-    },
-  }, { prefix = "<Leader>" })
-
-  nvim.create_user_command("TestSummary", function()
-    neotest.summary.toggle()
-  end, {})
-
-  -- nvim.create_user_command("TestAttach", function()
-  --   neotest.run.attach()
-  -- end, {})
-
-  nvim.create_user_command("TestStop", function()
-    neotest.run.stop()
-  end, {})
-
   local commands = {
-    -- {
-    --   display = "Run last run test",
-    --   callback = function()
-    --     neotest.run.run_last()
-    --   end,
-    --   repeatable = true,
-    -- },
-    {
+    last = {
+      display = "Re-run last test",
+      callback = function()
+        neotest.run.run_last()
+      end,
+      repeatable = true,
+    },
+    nearest = {
       display = "Run nearest test",
       callback = function()
         neotest.run.run()
       end,
       repeatable = true,
     },
-    {
+    debug_nearest = {
+      display = "Debug nearest test",
+      callback = function()
+        neotest.run.run({ vim.fn.expand("%"), strategy = "dap" })
+      end,
+      repeatable = true,
+    },
+    file = {
       display = "Run all tests in current file",
       callback = function()
         neotest.run.run(vim.fn.expand("%"))
       end,
     },
-    {
-      display = "Debug nearest test",
-      callback = function()
-        -- neotest.run.run({ strategy = "dap" })
-        neotest.run.run({ vim.fn.expand("%"), strategy = "dap" })
-      end,
-      repeatable = true,
-    },
-    {
+    stop = {
       display = "Stop nearest test",
       callback = function()
         neotest.run.stop()
       end,
       repeatable = false,
     },
-    {
+    summary = {
       display = "Toggle test summary",
       callback = function()
         neotest.summary.toggle()
       end,
       repeatable = true,
     },
-    {
+    output = {
       display = "Show test output",
       callback = function()
         neotest.output.open({ enter = true })
       end,
     },
+    output_panel = {
+      display = "Toggle output panel",
+      callback = function()
+        neotest.output_panel.toggle()
+      end,
+    },
   }
 
-  -- nvim.create_user_command("TestDebug", function()
-  --   neotest.run.run({ strategy = "dap" })
-  -- end, {})
+  local command_names = vim
+    .iter(commands)
+    :map(function(key)
+      return key
+    end)
+    :totable()
 
   nvim.create_user_command("Test", function(opts)
     local args = opts.fargs
 
-    if vim.tbl_isempty(args) or args[1] == "nearest" then
-      neotest.run.run()
-    elseif args[1] == "summary" then
-      neotest.summary.toggle()
-    elseif args[1] == "file" then
-      neotest.run.run(vim.fn.expand("%"))
-    elseif args[1] == "last" then
-      neotest.run.run_last()
-    elseif args[1] == "output" then
-      neotest.output.open({ enter = true })
-    elseif args[1] == "output_panel" then
-      neotest.output_panel.toggle()
+    local arg = args[1] or "nearest"
+
+    if commands[arg] then
+      commands[arg].callback()
     else
       vim.notify("Unknown command: " .. args[1], vim.log.levels.ERROR)
     end
   end, {
     nargs = "?",
     complete = function()
-      return { "nearest", "file", "summary", "last", "output", "output_panel" }
+      return command_names
     end,
   })
 
   key.map("[t", function()
     require("neotest").jump.prev()
-  end, { modes = "n", desc = "Move to previous test" })
+  end, { modes = "n", desc = "Jump to previous test" })
 
   key.map("]t", function()
     require("neotest").jump.next()
-  end, { modes = "n", desc = "Move to next test" })
+  end, { modes = "n", desc = "Jump to next test" })
 end
 
 return M
---
+++ lua/types.lua
@@ -0,0 +1,29 @@
+---@meta
+
+---@class vim.api.create_autocmd.callback.args
+---@field id number
+---@field event string
+---@field group number?
+---@field match string
+---@field buf number
+---@field file string
+---@field data any
+
+---@class vim.api.keyset.create_autocmd.opts: vim.api.keyset.create_autocmd
+---@field callback? fun(ev:vim.api.create_autocmd.callback.args):boolean?
+
+--- @param event any (string|array) Event(s) that will trigger the handler
+--- @param opts vim.api.keyset.create_autocmd
+--- @return integer
+function nvim.create_autocmd(event, opts) end
+
+
+---@param name string
+---@param opts vim.api.keyset.create_augroup
+--- @return integer
+function nvim.create_augroup(name, opts) end
+
+---@param name string
+---@param command any
+---@param opts vim.api.keyset.user_command
+function nvim.create_user_command(name, command, opts) end
