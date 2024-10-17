786902e706753cb70b5a759f387ce68e9ef6761f
Alex Wu <adwu@pm.me>
2024-10-17 09:54:59 -0700

    neotest in its own file

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,187 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "haydenmeade/neotest-jest",
+      -- "rouge8/neotest-rust",
+      "marilari88/neotest-vitest",
+      "antoinemadec/FixCursorHold.nvim",
+      "folke/trouble.nvim",
+    },
+    opts = {
+      log_level = vim.log.levels.TRACE,
+      -- NOTE: Can be a list of adapters like what neotest expects,
+      -- or a list of adapter names,
+      -- or a table of adapter names, mapped to adapter configs.
+      -- The adapter will then be automatically loaded with the config.
+      -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/test/core.lua
+      adapters = {
+        ["rustaceanvim.neotest"] = {},
+        ["neotest-jest"] = {
+          jestCommand = "yarn test",
+          jestConfigFile = "jest.config.js",
+          env = { DEBUG_PRINT_LIMIT = 20000 },
+          cwd = function(path)
+            return vim.fs.dirname(
+              vim.fs.find(
+                { "jest.config.js", "package.json" },
+                { upward = true, stop = vim.loop.os_homedir(), path = path }
+              )[1]
+            )
+          end,
+        },
+        ["neotest-vitest"] = {},
+      },
+      -- NOTE: Example for loading neotest-golang with a custom config
+      -- adapters = {
+      --   ["neotest-golang"] = {
+      --     go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+      --     dap_go_enabled = true,
+      --   },
+      -- },
+      icons = {
+        passed = " ✔",
+        running = " ",
+        failed = " ✖",
+        skipped = " ﰸ",
+        unknown = " ?",
+        non_collapsible = "─",
+        collapsed = "─",
+        expanded = "╮",
+        child_prefix = "├",
+        final_child_prefix = "╰",
+        child_indent = "│",
+        final_child_indent = " ",
+      },
+      status = { virtual_text = false },
+      output = { open_on_run = true },
+      quickfix = {
+        open = function()
+          require("trouble").open({ mode = "quickfix", focus = false })
+        end,
+      },
+    },
+    config = function(_, opts)
+      opts.consumers = opts.consumers or {}
+      -- Refresh and auto close trouble after running tests
+      ---@type neotest.Consumer
+      opts.consumers.trouble = function(client)
+        client.listeners.results = function(adapter_id, results, partial)
+          if partial then
+            return
+          end
+          local tree = assert(client:get_position(nil, { adapter = adapter_id }))
+
+          local failed = 0
+          for pos_id, result in pairs(results) do
+            if result.status == "failed" and tree:get_key(pos_id) then
+              failed = failed + 1
+            end
+          end
+          vim.schedule(function()
+            local trouble = require("trouble")
+            if trouble.is_open() then
+              trouble.refresh()
+              if failed == 0 then
+                trouble.close()
+              end
+            end
+          end)
+          return {}
+        end
+      end
+
+      if opts.adapters then
+        local adapters = {}
+        for name, config in pairs(opts.adapters or {}) do
+          if type(name) == "number" then
+            if type(config) == "string" then
+              config = require(config)
+            end
+            adapters[#adapters + 1] = config
+          elseif config ~= false then
+            local adapter = require(name)
+            if type(config) == "table" and not vim.tbl_isempty(config) then
+              local meta = getmetatable(adapter)
+              if adapter.setup then
+                adapter.setup(config)
+              elseif meta and meta.__call then
+                adapter(config)
+              else
+                error("Adapter " .. name .. " does not support setup")
+              end
+            end
+            adapters[#adapters + 1] = adapter
+          end
+        end
+        opts.adapters = adapters
+      end
+
+      require("neotest").setup(opts)
+      require("bombeelu.neotest").setup()
+    end,
+    keys = {
+      { "<leader>t", "", desc = "+test" },
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "Run all tests in file",
+      },
+      {
+        "<leader>ta",
+        function()
+          require("neotest").run.run(vim.loop.cwd())
+        end,
+        desc = "Run all test files",
+      },
+      {
+        "<leader>tn",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Run nearest test",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Toggle test summary",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output.open({ enter = true, auto_close = true })
+        end,
+        desc = "Show test output",
+      },
+      {
+        "<leader>tO",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "Toggle output panel",
+      },
+      {
+        "<leader>tS",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "Stop running tests",
+      },
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Re-run the last test",
+      },
+    },
+  },
+}
